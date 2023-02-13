c17d9b8977fd7dfc7d8bdb90137e9a9befa2824d
techlikeagirl <aewoodberry@ualr.edu>
2023-02-13 01:26:51 +0000

    added js info

--- unitsconversion/assets/scripts/unitsconversion.js
+++ unitsconversion/assets/scripts/unitsconversion.js
@@ -13,33 +13,59 @@ function calculate() {
         // Operator
         // Get the value associated with the operator that was checked (+, -, *, or /)
         var operator;
-        if (document.getElementById("AddOperator").checked) {
-            operator = document.getElementById("AddOperator").value;
+        if (document.getElementById("centunit").checked) {
+            operator = document.getElementById("centunit").value;
         }
-        if (document.getElementById("SubtractOperator").checked) {
-            operator = document.getElementById("SubtractOperator").value;
+        if (document.getElementById("meterunit").checked) {
+            operator = document.getElementById("meterunit").value;
         }
-        if (document.getElementById("MultiplyOperator").checked) {
-            operator = document.getElementById("MultiplyOperator").value;
+        if (document.getElementById("kilounit").checked) {
+            operator = document.getElementById("kilounit").value;
         }
-        if (document.getElementById("DivideOperator").checked) {
-            operator = document.getElementById("DivideOperator").value;
+        if (document.getElementById("inchunit").checked) {
+            operator = document.getElementById("inchunit").value;
         }
-        
-        // Operand 2
-        var operand2 = document.getElementById("Operand2").value;
-
-        CalculateResult(operand1, operator, operand2);
+        if (document.getElementById("feetunit").checked) {
+            operator = document.getElementById("feetunit").value;
+        }
+        if (document.getElementById("yardunit").checked) {
+            operator = document.getElementById("yardunit").value;
+        }
+        if (document.getElementById("mileunit").checked) {
+            operator = document.getElementById("mileunit").value;
+        }
+        if (document.getElementById("centunit1").checked) {
+            operator = document.getElementById("centunit").value;
+        }
+        if (document.getElementById("meterunit1").checked) {
+            operator = document.getElementById("meterunit").value;
+        }
+        if (document.getElementById("kilounit1").checked) {
+            operator = document.getElementById("kilounit").value;
+        }
+        if (document.getElementById("inchunit1").checked) {
+            operator = document.getElementById("inchunit").value;
+        }
+        if (document.getElementById("feetunit1").checked) {
+            operator = document.getElementById("feetunit").value;
+        }
+        if (document.getElementById("yardunit1").checked) {
+            operator = document.getElementById("yardunit").value;
+        }
+        if (document.getElementById("mileunit1").checked) {
+            operator = document.getElementById("mileunit").value;
+        }
+        CalculateResult(operand1, centunit, centunit1);
     }
 }
 
-async function CalculateResult(operand1, Unit, Unit1) {
+async function CalculateResult(operand1, centunit, centunit1) {
         
         // URL and method used with AJAX Call
         var myURL = "https://brucebauer.info/assets/ITEC3650/unitsconversion.php";
 
         /* AJAX calculator requires Operand1, Operator, and Operand2 */
-        myURL = myURL + "?Operand1=" + encodeURIComponent(operand1) + "&centunit=" + encodeURIComponent(operator) + "&centunit1=" + encodeURIComponent(operand2);
+        myURL = myURL + "?Operand1=" + encodeURIComponent(operand1) + "&centunit=" + encodeURIComponent(centunit) + "&centunit1=" + encodeURIComponent(centunit1);
 
         /* fetch the results */
         let myCalcObject = await fetch(myURL);
@@ -54,13 +80,22 @@ function clearform() {
     /* Set all of the form values to blank or false */
     document.getElementById("Operand1").value = "";
     document.getElementById("Operand1Msg").innerHTML = "";
-    document.getElementById("AddOperator").checked = false;
-    document.getElementById("SubtractOperator").checked = false;
-    document.getElementById("MultiplyOperator").checked = false;
-    document.getElementById("DivideOperator").checked = false;
-    document.getElementById("OperatorMsg").innerHTML = "";
-    document.getElementById("Operand2").value = "";
-    document.getElementById("Operand2Msg").innerHTML = "";
+    document.getElementById("centunit").checked = false;
+    document.getElementById("meterunit").checked = false;
+    document.getElementById("kilounit").checked = false;
+    document.getElementById("inchunit").checked = false;
+    document.getElementById("feetunit").checked = false;
+    document.getElementById("yardunit").checked = false;
+    document.getElementById("mileunit").checked = false;
+    document.getElementById("centunit1").checked = false;
+    document.getElementById("meterunit1").checked = false;
+    document.getElementById("kilounit1").checked = false;
+    document.getElementById("inchunit1").checked = false;
+    document.getElementById("feetunit1").checked = false;
+    document.getElementById("yardunit1").checked = false;
+    document.getElementById("mileunit1").checked = false;
+    document.getElementById("Uniterror").innerHTML = "";
+    document.getElementById("Unit1error").innerHTML = "";
     document.getElementById("Result").innerHTML = "";
 }
 
